0342444825016af29ea67ded0cfa5a4d8c629e38
Prakul Banandur <155788207+20prakulbanandur05@users.noreply.github.com>
2024-01-06 11:43:51 +0530

    Add files via upload

---
+++ javascript_presentation.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    Strings: ["Web Designer" , "Data Analyst" , "Professional Musician" , "Public Speaker"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    looped: true
+});
